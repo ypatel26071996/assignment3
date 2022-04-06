2d2529c7b1705e62936fa161c7ebd7192422ded2
ypatel26071996 <102184460+ypatel26071996@users.noreply.github.com>
2022-04-05 22:15:29 -0400

    Add Script update
    if else nesting changed to allow individual validation

--- add_script.js
+++ add_script.js
@@ -10,33 +10,57 @@ function addInfo(){
     let make=document.getElementById("make").value;
     let model=document.getElementById("model").value;
     let year=document.getElementById("year").value;
+    let status=true;
     if(fname.length==0){
         document.getElementById("fname_status").innerHTML="*Required";
-    }else if(lname.length==0){
+        status=false;
+    }
+    if(lname.length==0){
         document.getElementById("lname_status").innerHTML="*Required";
-    }else if(address.length==0){
+        status=false;
+    }
+    if(address.length==0){
         document.getElementById("address_status").innerHTML="*Required";
-    }else if(city.length==0){
+        status=false;
+    }
+     if(city.length==0){
         document.getElementById("city_status").innerHTML="*Required";
-    }else if(province.length==0){
+        status=false;
+    }
+    if(province.length==0){
         document.getElementById("province_status").innerHTML="*Required";
-    }else if(postalcode.length==0){
+        status=false;
+    }
+    if(postalcode.length==0){
         document.getElementById("postalcode_status").innerHTML="*Required";
-    }else if(phone.length==0){
+        status=false;
+    }
+     if(phone.length==0){
         document.getElementById("phone_status").innerHTML="*Required";
-    }else if(email.length==0){
+        status=false;
+    }
+     if(email.length==0){
         document.getElementById("email_status").innerHTML="*Required";
-    }else if(make.length==0){
+        status=false;
+    }
+     if(make.length==0){
         document.getElementById("make_status").innerHTML="*Required";
-    }else if(model.length==0){
+        status=false;
+    }
+     if(model.length==0){
         document.getElementById("model_status").innerHTML="*Required";
-    }else if(year.length==0){
+        status=false;
+    } if(year.length==0){
         document.getElementById("year_status").innerHTML="*Required";
-    }else if(/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/.test(phone)==false){
+    } if(/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/.test(phone)==false){
         document.getElementById("phone_status").innerHTML="*incorrect format";
-    }else if(/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(postalcode)==false){
+        status=false;
+    } if(/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(postalcode)==false){
         document.getElementById("postal_status").innerHTML="*Incorrect format";
-    }else{
+        status=false;
+    }
+    
+    if(status){
         const items = (() => {
             const fieldValue = localStorage.getItem('items');
             return fieldValue === null
